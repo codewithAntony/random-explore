2d64168a21fa09a06d76670df898210242415fab
Antony <antonymurithi51@gmail.com>
2025-09-06 16:26:29 +0300

    created the header with the logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import Header from './components/Header'
 
 
 function App() {
 
   return (
-    <div className='bg-red-300'>
-      home
+    <div className="bg-[#F2F6FF]">
+      <Header />
     </div>
       
   )
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='max-w-6xl mx-auto px-5'>
+      <div className=''>
+            <span className='text-[#51e2f5] font-bold uppercase text-2xl md:text-3xl lg:text-3xl'>Random</span>
+            <span className='font-bold uppercase text-2xl md:text-3xl lg:text-3xl'>Explore</span>
+      </div>
+    </div>
+  )
+}
+
+export default Header
